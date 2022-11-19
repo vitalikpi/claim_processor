0ee2e2057d12e948f2cf45f082f82c111ab36128
vpianykh <vitaliy.pianykh@gmail.com>
2022-11-18 23:05:50 -0500

    implement basic processing without storing

--- app/main.py
+++ app/main.py
@@ -1,13 +1,39 @@
+from typing import Union, List
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+import uuid
+
+
+class Claim(BaseModel):
+    service_date: str
+    submitted_procedure: str
+    quadrant: Union[str, None] = None
+    plan_or_Group_no: str
+    subscriber_no: str
+    provider_npi: str
+    provider_fees: float
+    allowed_fees: float
+    member_coinsurance: float
+    member_copay: float
+
+
+class ProcessingResponse(BaseModel):
+    uid: str
+    net_fee: float
+
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/claim_process")
+async def claim_process(claims: List[Claim]) -> ProcessingResponse:
+    result = []
+    for claim in claims:
+        net_fee = claim.provider_fees + claim.member_coinsurance + claim.member_copay - claim.allowed_fees
+        result.append(ProcessingResponse(uid=str(uuid.uuid1()), net_fee=net_fee))
+    return result
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+
