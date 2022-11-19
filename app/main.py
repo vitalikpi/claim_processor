69fcc5eabc7257ac57e04a4a808bc31c701d2f5f
vpianykh <vitaliy.pianykh@gmail.com>
2022-11-19 00:51:22 -0500

    add some doc strings

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import uuid
 from typing import Union, List, Optional
+from datetime import date, datetime, time, timedelta
 
 import pymysql
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, validator, NonNegativeFloat
 from sqlmodel import Field, Session, SQLModel, create_engine
 
 
@@ -23,17 +24,64 @@ class ClaimModel(SQLModel, table=True):
     member_copay: float
 
 
+"""
+Claim information.
+"""
 class Claim(BaseModel):
-    service_date: str
-    submitted_procedure: str
+    """
+    A date when the service was provided
+    """
+    service_date: date
+
+    """
+    A code of submitted procedure. Must always start with 'D'
+    """
+    submitted_procedure: str = Field(..., min_length=1)
+
+    """
+    A quadrant. The only optional field
+    """
     quadrant: Union[str, None] = None
-    plan_or_group_no: str
-    subscriber_no: str
-    provider_npi: str
-    provider_fees: float
-    allowed_fees: float
-    member_coinsurance: float
-    member_copay: float
+
+    """
+    Plan / group #
+    """
+    plan_or_group_no: str = Field(..., min_length=1)
+
+    """
+    Subscriber #
+    """
+    subscriber_no: str = Field(..., min_length=1)
+
+    """
+    Provider NPI
+    """
+    provider_npi: str = Field(..., min_length=10, max_length=10)
+
+    """
+    Provider fees
+    """
+    provider_fees: NonNegativeFloat
+
+    "Allowed fees"
+    allowed_fees: NonNegativeFloat
+
+    """
+    Member coinsurance
+    """
+    member_coinsurance: NonNegativeFloat
+
+    """
+    Member copay
+    """
+    member_copay: NonNegativeFloat
+
+    @validator('submitted_procedure')
+    def submitted_procedure_always_starts_with_d(cls, v):
+        if v[0] != "D":
+            raise ValueError('submitted_procedure must always start with D')
+        return v
+
 
 
 class ProcessingResponse(BaseModel):
@@ -47,6 +95,10 @@ SQLModel.metadata.create_all(engine)
 app = FastAPI()
 
 
+"""
+Processes and stores list of claims provided in the body.
+In response returns a unique id for each claim and it's bet fee.
+"""
 @app.post("/claim_process")
 async def claim_process(claims: List[Claim]) -> ProcessingResponse:
     result = []
